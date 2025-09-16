a4e2cc689c4b4b2704959dae90168ac462d46fda
Yassine Dahmane <144846300+KaiserTheForce@users.noreply.github.com>
2025-09-16 20:45:12 +0000

    chore: ignore eslint/ts errors during builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Ne bloque pas le build si ESLint trouve des erreurs
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // (optionnel) ne bloque pas le build si TS voit des erreurs
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
